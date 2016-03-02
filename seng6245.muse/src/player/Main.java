46d80ff999d02e5676fd7d0f753d74798e8b60bf
thetrick <toddghetrick@gmail.com>
2016-03-02 18:45:38 -0500

    Implementing Lexer and Parser

--- seng6245.muse/src/player/Main.java
+++ seng6245.muse/src/player/Main.java
@@ -1,10 +1,12 @@
 package player;
 
+import java.util.ArrayList;
+
 /**
  * Main entry point of your application.
  */
 public class Main {
-
+	private static final int selectedFileIndex = 1;
 	/**
 	 * Plays the input file using Java MIDI API and displays
 	 * header information to the standard output stream.
@@ -15,6 +17,23 @@ public class Main {
 	 * @param file the name of input abc file
 	 */
 	public static void play(String file) {
-		// YOUR CODE HERE
+
 	}
+	
+	public static void main(String[] args) {
+		ArrayList<String> abcfiles = new ArrayList<String>();
+		abcfiles.add("sample_abc/fur_elise.abc");
+		abcfiles.add("sample_abc/invention.abc");
+		abcfiles.add("sample_abc/little_night_music.abc");
+		abcfiles.add("sample_abc/paddy.abc");
+		abcfiles.add("sample_abc/piece1.abc");
+		abcfiles.add("sample_abc/piece2.abc");
+		abcfiles.add("sample_abc/prelude.abc");
+		abcfiles.add("sample_abc/scale.abc");
+		abcfiles.add("sample_abc/test.abc");
+		abcfiles.add("sample_abc/SampleChordsandTuplets.abc");
+		abcfiles.add("sample_abc/test2.abc");
+		String abcfile = abcfiles.get(selectedFileIndex);
+		play(abcfile);
+	}	
 }
